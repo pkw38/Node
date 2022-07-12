8eeb84d008f2ef995a1b9bf1220f27006db8fb6e
pkw38 <kyungwon42094@gmail.com>
2022-07-12 09:22:16 +0900

    update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World!!!!!!!!!!!!!!!')
 })
 
 app.get('/store', function (req, res){
-    res.send('치킨')
+    res.send('치킨!!!!!!!!!!!!!!!!!')
 })
 
 app.listen(3000)
